5d3395340b9b1028f9892db7e3e64ed1bfe1f806
nathanq95 <nathanquiocson@gmail.com>
2021-07-12 14:14:46 -0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+/*
+  this app component is the highest level component.
+  similar to the app component in react, where it contains all the child components
+*/
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor() {
+    this.name = 'yeeet';
+  }
+
+  name: string;
   title = 'intro';
+
+  
 }
